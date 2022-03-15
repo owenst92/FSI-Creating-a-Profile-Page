04e56dd9e135cd36f144ac608021bce9cfdb4a78
owenst92 <owenst92@gmail.com>
2022-03-14 21:05:57 -0400

    made changes

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+content.append(header)
